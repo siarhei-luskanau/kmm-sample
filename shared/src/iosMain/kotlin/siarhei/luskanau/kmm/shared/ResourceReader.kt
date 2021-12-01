3196deb0022592969fc743461a48b49b0d10dbf2
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2021-12-01 23:21:10 +0300

    Updated iOS target of ResourceReader

--- shared/src/iosMain/kotlin/siarhei/luskanau/kmm/shared/ResourceReader.kt
+++ shared/src/iosMain/kotlin/siarhei/luskanau/kmm/shared/ResourceReader.kt
@@ -1,14 +1,19 @@
 package siarhei.luskanau.kmm.shared
 
 import platform.Foundation.NSBundle
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSString
+import platform.Foundation.NSUTF8StringEncoding
+import platform.Foundation.create
 
 actual class ResourceReader(
     val bundle: NSBundle
 ) {
 
     actual fun readResource(name: String): String? =
-        "{\n" +
-            "  \"name\": \"kotlinx.serialization\",\n" +
-            "  \"language\": \"${bundle}\"\n" +
-            "}\n"
+        bundle.pathForResource(name, null)?.let { path ->
+            NSFileManager.defaultManager.contentsAtPath(path)?.let { nsData ->
+                NSString.create(nsData, NSUTF8StringEncoding) as String?
+            }
+        }
 }
